b01a37dc12decc38d52939b44c5121310bd6b67b
ivanFlor3s <ifloresmanc96@gmail.com>
2023-01-01 22:14:47 -0300

    Navbar styles + agrego showMenu

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+    showMenu: boolean = false;
 
+    toggleMenu() {
+        this.showMenu = !this.showMenu;
+    }
 }
