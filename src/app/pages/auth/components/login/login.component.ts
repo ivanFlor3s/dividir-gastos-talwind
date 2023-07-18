72cdeeeba5a4ed3ac476212ed374c993d227cc82
ivanFlor3s <ifloresmanc96@gmail.com>
2023-07-17 23:16:21 -0300

    feat: :sparkles: Form en login
    login formgroup + validaciones

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+    private _fb = inject(FormBuilder);
 
+    loginForm: FormGroup = this._fb.group({
+        email: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+    });
 }
