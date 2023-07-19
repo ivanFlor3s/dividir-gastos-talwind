77fe2898ece0a9dd3bfa6ad568a409eca917b551
ivanFlor3s <ifloresmanc96@gmail.com>
2023-07-18 22:06:57 -0300

    refactor: :recycle: quito import sin uso

--- src/app/pages/auth/components/register/register.component.ts
+++ src/app/pages/auth/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
