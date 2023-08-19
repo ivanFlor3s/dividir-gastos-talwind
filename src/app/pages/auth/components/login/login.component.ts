7dccd38c4f4069e1f3d9c6f276a94c589fc598af
ivanFlor3s <ifloresmanc96@gmail.com>
2023-08-19 00:27:13 -0300

    feat: login success redirect to dashboard and swal on error

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '@core/services';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-login',
@@ -10,6 +12,7 @@ import { AuthService } from '@core/services';
 export class LoginComponent {
     private _fb = inject(FormBuilder);
     private _authService = inject(AuthService);
+    private _router = inject(Router);
 
     loginForm: FormGroup = this._fb.group({
         email: ['', Validators.required],
@@ -23,6 +26,17 @@ export class LoginComponent {
 
         this._authService
             .login(this.loginForm.value.email, this.loginForm.value.password)
-            .subscribe((res) => console.log(res));
+            .subscribe({
+                next: (_) => {
+                    this._router.navigate(['/dashboard']);
+                },
+                error: (error) => {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error al iniciar sesión',
+                        text: error.error.error,
+                    });
+                },
+            });
     }
 }
