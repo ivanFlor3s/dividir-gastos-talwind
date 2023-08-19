1130d86d11ea7cd16b1f56781bd6f71200bb61a2
ivanFlor3s <ifloresmanc96@gmail.com>
2023-08-18 22:34:19 -0300

    feat: login request from form submit

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@core/services';
 
 @Component({
     selector: 'app-login',
@@ -8,9 +9,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
     private _fb = inject(FormBuilder);
+    private _authService = inject(AuthService);
 
     loginForm: FormGroup = this._fb.group({
         email: ['', Validators.required],
         password: ['', [Validators.required, Validators.minLength(6)]],
     });
+
+    public submit() {
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        this._authService
+            .login(this.loginForm.value.email, this.loginForm.value.password)
+            .subscribe((res) => console.log(res));
+    }
 }
