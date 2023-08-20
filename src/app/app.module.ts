2ce875eb62ad52616cc23547a5a17ee903896720
ivanFlor3s <ifloresmanc96@gmail.com>
2023-08-19 23:16:16 -0300

    Configure app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Modules
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { ClickOutsideDirective } from './directives/click-outside.directive';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AgregarGastoComponent } from './pages/dashboard/components/modals/agregar-gasto/agregar-gasto.component';
-import { UsersListComponent } from './components/users-list/users-list.component';
-import { GroupCardComponent } from './pages/dashboard/components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorsModule } from '../core/errors/errors.module';
 import { HttpClientModule } from '@angular/common/http';
+
+//Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { GroupCardComponent } from './pages/dashboard/components';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AgregarGastoComponent } from './pages/dashboard/components/modals/agregar-gasto/agregar-gasto.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+
+//Ngxs
+import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { AppState } from '@core/state/app-state/app.state';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
     declarations: [
@@ -32,6 +41,14 @@ import { HttpClientModule } from '@angular/common/http';
         FormsModule,
         ErrorsModule.forRoot({ required: 'Este campo es requerido' }),
         HttpClientModule,
+        NgxsModule.forRoot([AppState], {
+            developmentMode: process.env['PRODUCTION'] != '1',
+        }),
+        NgxsReduxDevtoolsPluginModule.forRoot(),
+        NgxsStoragePluginModule.forRoot({
+            key: [AppState],
+            storage: StorageOption.LocalStorage,
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent],
