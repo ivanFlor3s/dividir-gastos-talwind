e64503083cf03c4ab6bd6586453b7f1c65d82a07
ivanFlor3s <ifloresmanc96@gmail.com>
2023-08-26 20:37:00 -0300

    Add click outside directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent, NavbarComponent } from './components';
+import { ClickOutsideDirective } from './directives';
 
 @NgModule({
-    declarations: [DropdownComponent, NavbarComponent],
+    declarations: [DropdownComponent, NavbarComponent, ClickOutsideDirective],
     imports: [CommonModule],
-    exports: [DropdownComponent, NavbarComponent],
+    exports: [DropdownComponent, NavbarComponent, ClickOutsideDirective],
     providers: [],
 })
 export class SharedModule {}
