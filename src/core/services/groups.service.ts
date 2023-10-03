eb9d7fc83ce5968fd91d7431b99539e3a0f74fa8
ivanFlor3s <ifloresmanc96@gmail.com>
2023-10-03 00:09:50 -0300

    get group from service (api req)

--- src/core/services/groups.service.ts
+++ src/core/services/groups.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { FilterParams } from '@app/interfaces';
 import { GroupVM } from '@app/models/view-models';
 import { Observable } from 'rxjs';
 
@@ -10,9 +9,9 @@ import { Observable } from 'rxjs';
 export class GroupsService {
     private _http = inject(HttpClient);
 
-    getGroups(filter: FilterParams): Observable<GroupVM[]> {
+    getGroups(name: string): Observable<GroupVM[]> {
         return this._http.get<GroupVM[]>(`${process.env['API_URL']}/group`, {
-            params: { ...filter },
+            params: { name },
         });
     }
 }
