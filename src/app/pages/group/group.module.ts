9e11e732a74c50fa75dcf644438a7333652f5e90
ivanFlor3s <ifloresmanc96@gmail.com>
2024-03-14 00:16:58 -0300

    testing app user list (only arrows)

--- src/app/pages/group/group.module.ts
+++ src/app/pages/group/group.module.ts
@@ -4,9 +4,10 @@ import { GroupContainerComponent } from './container';
 import { GroupRoutingModule } from './group.routing';
 import { AddSpentComponent, ItemDetailComponent } from './components';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { SharedModule } from '@app/shared/shared.module';
 
 @NgModule({
-    imports: [CommonModule, GroupRoutingModule, NgxSpinnerModule],
+    imports: [CommonModule, GroupRoutingModule, NgxSpinnerModule, SharedModule],
     declarations: [
         GroupContainerComponent,
         ItemDetailComponent,
